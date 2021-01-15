afc7c0c41386460800b0fbc55b507efad0ef6e86
tundeiness <tundeiness@yahoo.co.uk>
2021-01-15 13:12:01 +0100

    import components in App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Components/Header';
+import Hero from './Components/Hero';
+import Advert from './Components/Advert';
+import Testimonial from './Components/Testimonial';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import './styles.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="App" id="main">
+        <Hero />
+        <Advert />
+        <Testimonial />
+        <Contact />
+        <Footer />
+        {/* <p>Some placeholder</p> */}
+      </div>
+    </>
   );
 }
-
-export default App;
