53234fde5c057e93bfd4076717460f4df8d81178
tundeiness <tundeiness@yahoo.co.uk>
2021-01-15 13:03:34 +0100

    create Advert.js

---
+++ src/Components/Advert.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Advert = () => (
+  <div className="container-fluid" id="advert_section">
+    <div className="row">
+      <div className="col-md-6 col-12 d-flex flex-column flex-lg-column advert_statement">
+        <p className="advert_statement_h ">
+          Aenean sit amet aliquam quam. Phasellus mi nisi, scelerisque eget
+          cursus id, porta nec arcu.
+          {' '}
+        </p>
+        <p className="advert_statement_p">
+          In non orci elementum, ullamcorper ante vitae, lobortis enim. Praesent
+          dui arcu, tempus at venenatis in, placerat quis purus. Maecenas
+          condimentum lacinia neque, interdum aliquet leo faucibus gravida.
+          Vestibulum faucibus a urna et blandit. In non orci elementum,
+          ullamcorper ante vitae, lobortis enim. Praesent dui arcu, tempus at
+          venenatis in, placerat quis purus. Maecenas condimentum lacinia neque,
+          interdum aliquet leo faucibus gravida. Vestibulum faucibus a urna et
+          blandit.
+        </p>
+      </div>
+      <div className="col-md-6 col-12 advert_image pr-0">
+        <img
+          className="img-fluid"
+          src="https://user-images.githubusercontent.com/25479050/103773808-efaf7000-502b-11eb-859a-c00e77518e87.png"
+          alt="product distribution worldwide"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+export default Advert;
