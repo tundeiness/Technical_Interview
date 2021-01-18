c94d747c66f1c0113772edecafeb8ba6d510da30
tundeiness <tundeiness@yahoo.co.uk>
2021-01-18 15:59:19 +0100

    complete footer section refactor for large screens

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,17 +6,16 @@ const Footer = () => (
       <div className="row flex-column align-items-lg-center flex-md-row">
         <div className="col-12 col-md-6 copyright">
           {' '}
-          <p>&copy; 2021 Company. All Rights Reserved</p>
+          <span>&copy; 2021 Company. All Rights Reserved</span>
         </div>
         <div className="col-md-6 col-12 d-flex flex-row justify-content-lg-end socials">
           <div className="socials_text_wrapper mr-lg-0">
-            <p className="social_text d-inline ">Follow us on:</p>
+            <span className="social_text d-inline ">Follow us on:</span>
             <i className="lni lni-facebook-original facebook" />
             <i className="lni lni-linkedin-original linkedin" />
             <i className="lni lni-twitter-original twitter" />
             <i className="lni lni-envelope envelope" />
           </div>
-          <div className="d-inline social_icon_wrapper" />
         </div>
       </div>
     </div>
