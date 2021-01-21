5843c4a397402637b49f4cc3a8acb4faf0bbbfa9
tundeiness <tundeiness@yahoo.co.uk>
2021-01-21 13:38:48 +0100

    bump header styling on medium screens

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,11 +4,11 @@ const Footer = () => (
   <div className="container-fluid footer_wrapper">
     <div className="container" id="footer">
       <div className="row flex-column align-items-lg-center flex-md-row">
-        <div className="col-12 col-md-6 copyright">
+        <div className="col-12 d-flex justify-content-center col-md-6 copyright">
           {' '}
           <span>&copy; 2021 Company. All Rights Reserved.</span>
         </div>
-        <div className="col-md-6 col-12 d-flex flex-row justify-content-lg-end socials">
+        <div className="col-md-6 col-12 d-flex flex-row justify-content-center justify-content-lg-end socials">
           <div className="socials_text_wrapper mr-lg-0">
             <span className="social_text d-inline ">Follow us on:</span>
             <i className="lni lni-facebook-original facebook" />
